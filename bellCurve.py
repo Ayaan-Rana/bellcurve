336c0c68579dfbf7879017d09debf4e4f831c97f
Ayaan-Rana <71192735+Ayaan-Rana@users.noreply.github.com>
2021-07-12 01:20:03 -0500

    Add files via upload

---
+++ bellCurve.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv
+import plotly.figure_factory as ff
+
+df = pd.read_csv('data.csv')
+fig = ff.create_distplot([df['Avg Rating'].tolist()], ['Rating average'], show_hist=False)
+fig.show()
